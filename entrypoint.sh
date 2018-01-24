1672fc45a07a63fcb24c42fe1ceb215e80cb1719
Lars <elgohr@users.noreply.github.com>
2018-01-24 12:24:19 +0100

    Moving hostname to variable, due to debugging.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+export HOSTNAME=$(echo "$VCAP_APPLICATION" | cut -d'[' -f2 | cut -d']' -f1 | tr -d '"')
+
 ${LCSRV_HOME}/bin/license-server.sh configure \
 		--listen 0.0.0.0 \
 		--port 8111 \
-		--jetty.virtualHosts.names=$(echo "$VCAP_APPLICATION" | cut -d'[' -f2 | cut -d']' -f1) \
+		--jetty.virtualHosts.names=$HOSTNAME \
 		--logs-dir ${LCSRV_HOME}/logs \
 		--temp-dir ${LCSRV_HOME}/temp
 
