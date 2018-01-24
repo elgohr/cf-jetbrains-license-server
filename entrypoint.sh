3fcb0ddeac4154413d1c49dfb430368efa2e5b7f
Lars <elgohr@users.noreply.github.com>
2018-01-24 11:26:45 +0100

    Removing Access config

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ ${LCSRV_HOME}/bin/license-server.sh configure \
 		--port 8111 \
 		--jetty.virtualHosts.names=$(echo "$VCAP_APPLICATION" | cut -d'[' -f2 | cut -d']' -f1) \
 		--logs-dir ${LCSRV_HOME}/logs \
-		--temp-dir ${LCSRV_HOME}/temp \
-		--access.config=file:${LCSRV_HOME}/access-config.json
+		--temp-dir ${LCSRV_HOME}/temp
 
 exec ${LCSRV_HOME}/bin/license-server.sh run
