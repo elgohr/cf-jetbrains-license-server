3341d94bfef22a948ba617ce3f7ae2ff3a1acafd
Lars Gohr <larsgohr@gmail.com>
2018-02-11 13:40:30 +0100

    Fix path to register binary

--- register.sh
+++ register.sh
@@ -3,5 +3,5 @@ if [ ! -z "$USER" ] && [ ! -z "$PASSWORD" ] && [ ! -z "$SERVER_NAME" ]
 then
   sleep 30
   echo "Trying to register https://$HOSTNAME with $USER as $SERVER_NAME"
-  exec ./register "https://$HOSTNAME" "$USER" "$PASSWORD" "$SERVER_NAME"
+  exec $LCSRV_HOME/register "https://$HOSTNAME" "$USER" "$PASSWORD" "$SERVER_NAME"
 fi
