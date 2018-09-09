bf0d5b8f32c2dd9e7022a76e0f5bca6021eb1519
Lars Gohr <larsgohr@gmail.com>
2018-09-09 07:09:59 +0200

    Add parameter to define registration url hostname (BREAKING!)

--- register.sh
+++ register.sh
@@ -2,12 +2,12 @@
 if [ ! -z "$JETBRAINS_USERNAME" ] && [ ! -z "$JETBRAINS_PASSWORD" ] && [ ! -z "$SERVER_NAME" ]
 then
   sleep 30
-  echo "Trying to register https://$HOSTNAME with $JETBRAINS_USERNAME as $SERVER_NAME"
+  echo "Trying to register https://$SERVER_HOSTNAME with $JETBRAINS_USERNAME as $SERVER_NAME"
   if [ ! -z "$SERVER_USERNAME" ] && [ ! -z "$SERVER_PASSWORD" ]
   then
     echo "Using authentication"
-    exec $LCSRV_HOME/register "https://$SERVER_USERNAME:$SERVER_PASSWORD@$HOSTNAME/register" "$JETBRAINS_USERNAME" "$JETBRAINS_PASSWORD" "$SERVER_NAME"
+    exec $LCSRV_HOME/register "https://$SERVER_USERNAME:$SERVER_PASSWORD@$SERVER_HOSTNAME/register" "$JETBRAINS_USERNAME" "$JETBRAINS_PASSWORD" "$SERVER_NAME"
   else
-    exec $LCSRV_HOME/register "https://$HOSTNAME/register" "$JETBRAINS_USERNAME" "$JETBRAINS_PASSWORD" "$SERVER_NAME"
+    exec $LCSRV_HOME/register "https://$SERVER_HOSTNAME/register" "$JETBRAINS_USERNAME" "$JETBRAINS_PASSWORD" "$SERVER_NAME"
   fi
 fi
