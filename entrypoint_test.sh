20a19d6c72c5411aec566481d6c7d3aa4d08de9d
Lars Gohr <larsgohr@gmail.com>
2018-10-02 19:36:49 +0200

    Use dedicated user for running the server

--- entrypoint_test.sh
+++ entrypoint_test.sh
@@ -7,8 +7,8 @@ function clean() {
 function itStartsUpTheServerWithOneRoute() {
   clean
   export VCAP_APPLICATION='{"instance_id":"fe98dc76ba549876543210abcd1234","instance_index":0,"host":"0.0.0.0","port":61857,"started_at":"2013-08-1200:05:29 +0000","started_at_timestamp":1376265929,"start":"2013-08-12 00:05:29+0000","state_timestamp":1376265929,"limits":{"mem":512,"disk":1024,"fds":16384},"application_version":"ab12cd34-5678-abcd-0123-abcdef987654","application_name":"jb-testserver","application_uris":["myfirst.route"],"version":"ab12cd34-5678-abcd-0123-abcdef987654","name":"my-app","uris":["myfirst.route"],"users":null}'
-  result=$(exec ./entrypoint.sh)
-  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route --temp-dir //temp
+  result=$(exec ${USER_HOME}/entrypoint.sh)
+  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route --temp-dir /home/jetbrains/license-server/temp
 Called mock with: run
 Called mock with: run" ]; then
     echo "Does not start up the server as expected: $result"
@@ -19,8 +19,8 @@ Called mock with: run" ]; then
 function itStartsUpTheServerWithMultipleRoutes() {
   clean
   export VCAP_APPLICATION='{"instance_id":"fe98dc76ba549876543210abcd1234","instance_index":0,"host":"0.0.0.0","port":61857,"started_at":"2013-08-1200:05:29 +0000","started_at_timestamp":1376265929,"start":"2013-08-12 00:05:29+0000","state_timestamp":1376265929,"limits":{"mem":512,"disk":1024,"fds":16384},"application_version":"ab12cd34-5678-abcd-0123-abcdef987654","application_name":"jb-testserver","application_uris":["myfirst.route","mysecond.route"],"version":"ab12cd34-5678-abcd-0123-abcdef987654","name":"my-app","uris":["myfirst.route","mysecond.route"],"users":null}'
-  result=$(exec ./entrypoint.sh)
-  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir //temp
+  result=$(exec ${USER_HOME}/entrypoint.sh)
+  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir /home/jetbrains/license-server/temp
 Called mock with: run
 Called mock with: run" ]; then
     echo "Does not start up the server as expected: $result"
@@ -31,8 +31,8 @@ Called mock with: run" ]; then
 function itUsesApplicationUris() {
   clean
   export VCAP_APPLICATION='{"someOtherArray":["somethingElse"],"application_uris":["myfirst.route","mysecond.route"],"uris":["not.this.route","not.this.route2"]}'
-  result=$(exec ./entrypoint.sh)
-  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir //temp
+  result=$(exec ${USER_HOME}/entrypoint.sh)
+  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir /home/jetbrains/license-server/temp
 Called mock with: run
 Called mock with: run" ]; then
     echo "Didn't use the application_uris: $result"
