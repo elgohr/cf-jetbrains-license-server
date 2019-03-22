067c0ebc1d039f151f3c7db5533e31dd195fb585
Lars Gohr <larsgohr@gmail.com>
2019-03-22 20:19:21 +0100

    Add option for using a proxy server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,26 @@ set -e
 
 export HOSTNAME=$(echo "$VCAP_APPLICATION" | jq '.application_uris|@csv' | tr -d '"' | tr -d '\\')
 
+export PROXY=""
+if [ ! -z "$HTTP_PROXYHOST" ] && [ ! -z "$HTTP_PROXYPORT" ]; then
+    PROXY="-J-Dhttp.proxyHost $HTTP_PROXYHOST -J-Dhttp.proxyPort $HTTP_PROXYPORT"
+    if [ ! -z "$HTTP_PROXYUSER" ] && [ ! -z "$HTTP_PROXYPASSWORD" ]; then
+        PROXY="$PROXY -J-Dhttp.proxyUser $HTTP_PROXYUSER -J-Dhttp.proxyPassword $HTTP_PROXYPASSWORD"
+    fi
+fi
+if [ ! -z "$HTTPS_PROXYHOST" ] && [ ! -z "$HTTPS_PROXYPORT" ]; then
+    PROXY="$PROXY -J-Dhttps.proxyHost $HTTPS_PROXYHOST -J-Dhttps.proxyPort $HTTPS_PROXYPORT"
+    if [ ! -z "$HTTPS_PROXYUSER" ] && [ ! -z "$HTTPS_PROXYPASSWORD" ]; then
+        PROXY="$PROXY -J-Dhttps.proxyUser $HTTPS_PROXYUSER -J-Dhttps.proxyPassword $HTTPS_PROXYPASSWORD"
+    fi
+fi
+
 ${USER_HOME}/license-server/bin/license-server.sh configure \
 		--listen 0.0.0.0 \
 		--port 8111 \
-		--jetty.virtualHosts.names=$HOSTNAME \
+		--jetty.virtualHosts.names=${HOSTNAME} \
 		--temp-dir ${USER_HOME}/license-server/temp \
+		${PROXY} \
 		${CUSTOM_OPTIONS}
 
 exec ${USER_HOME}/register.sh run &
--- entrypoint_test.sh
+++ entrypoint_test.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 
 function clean() {
   unset VCAP_APPLICATION
+  unset HTTP_PROXYHOST
+  unset HTTP_PROXYPORT
+  unset HTTP_PROXYUSER
+  unset HTTP_PROXYPASSWORD
+  unset HTTPS_PROXYHOST
+  unset HTTPS_PROXYPORT
+  unset HTTPS_PROXYUSER
+  unset HTTPS_PROXYPASSWORD
 }
 
 function itStartsUpTheServerWithOneRoute() {
@@ -40,6 +48,66 @@ Called mock with: run" ]; then
   fi
 }
 
+function itCommunicatesViaHttpProxyIfProvided() {
+  clean
+  export HTTP_PROXYHOST='http://myCompany.proxy'
+  export HTTP_PROXYPORT='8080'
+  export VCAP_APPLICATION='{"someOtherArray":["somethingElse"],"application_uris":["myfirst.route","mysecond.route"],"uris":["not.this.route","not.this.route2"]}'
+  result=$(exec ${USER_HOME}/entrypoint.sh)
+  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir /home/jetbrains/license-server/temp -J-Dhttp.proxyHost http://myCompany.proxy -J-Dhttp.proxyPort 8080
+Called mock with: run
+Called mock with: run" ]; then
+    echo "Didn't use the http proxy server: $result"
+    exit 1
+  fi
+}
+
+function itCommunicatesViaSecuredHttpProxyIfProvided() {
+  clean
+  export HTTP_PROXYUSER='myUser'
+  export HTTP_PROXYPASSWORD='myPassword'
+  export HTTP_PROXYHOST='http://myCompany.proxy'
+  export HTTP_PROXYPORT='8080'
+  export VCAP_APPLICATION='{"someOtherArray":["somethingElse"],"application_uris":["myfirst.route","mysecond.route"],"uris":["not.this.route","not.this.route2"]}'
+  result=$(exec ${USER_HOME}/entrypoint.sh)
+  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir /home/jetbrains/license-server/temp -J-Dhttp.proxyHost http://myCompany.proxy -J-Dhttp.proxyPort 8080 -J-Dhttp.proxyUser myUser -J-Dhttp.proxyPassword myPassword
+Called mock with: run
+Called mock with: run" ]; then
+    echo "Didn't use the secured http proxy server: $result"
+    exit 1
+  fi
+}
+
+function itCommunicatesViaHttpsProxyIfProvided() {
+  clean
+  export HTTPS_PROXYHOST='https://myCompanys.proxy'
+  export HTTPS_PROXYPORT='8443'
+  export VCAP_APPLICATION='{"someOtherArray":["somethingElse"],"application_uris":["myfirst.route","mysecond.route"],"uris":["not.this.route","not.this.route2"]}'
+  result=$(exec ${USER_HOME}/entrypoint.sh)
+  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir /home/jetbrains/license-server/temp -J-Dhttps.proxyHost https://myCompanys.proxy -J-Dhttps.proxyPort 8443
+Called mock with: run
+Called mock with: run" ]; then
+    echo "Didn't use the https proxy server: $result"
+    exit 1
+  fi
+}
+
+function itCommunicatesViaSecuredHttpsProxyIfProvided() {
+  clean
+  export HTTPS_PROXYUSER='myUser'
+  export HTTPS_PROXYPASSWORD='myPassword'
+  export HTTPS_PROXYHOST='https://myCompanys.proxy'
+  export HTTPS_PROXYPORT='8443'
+  export VCAP_APPLICATION='{"someOtherArray":["somethingElse"],"application_uris":["myfirst.route","mysecond.route"],"uris":["not.this.route","not.this.route2"]}'
+  result=$(exec ${USER_HOME}/entrypoint.sh)
+  if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir /home/jetbrains/license-server/temp -J-Dhttps.proxyHost https://myCompanys.proxy -J-Dhttps.proxyPort 8443 -J-Dhttps.proxyUser myUser -J-Dhttps.proxyPassword myPassword
+Called mock with: run
+Called mock with: run" ]; then
+    echo "Didn't use the secured https proxy server: $result"
+    exit 1
+  fi
+}
+
 function itAddsCustomOptionsApplicationUris() {
   clean
   export CUSTOM_OPTIONS='THIS_IS_CUSTOM'
@@ -48,7 +116,7 @@ function itAddsCustomOptionsApplicationUris() {
   if [ "$result" != "Called mock with: configure --listen 0.0.0.0 --port 8111 --jetty.virtualHosts.names=myfirst.route,mysecond.route --temp-dir /home/jetbrains/license-server/temp THIS_IS_CUSTOM
 Called mock with: run
 Called mock with: run" ]; then
-    echo "Didn't use the application_uris: $result"
+    echo "Didn't add custom options: $result"
     exit 1
   fi
 }
@@ -56,4 +124,8 @@ Called mock with: run" ]; then
 itStartsUpTheServerWithOneRoute
 itStartsUpTheServerWithMultipleRoutes
 itUsesApplicationUris
+itCommunicatesViaHttpProxyIfProvided
+itCommunicatesViaHttpsProxyIfProvided
+itCommunicatesViaSecuredHttpProxyIfProvided
+itCommunicatesViaSecuredHttpsProxyIfProvided
 itAddsCustomOptionsApplicationUris
