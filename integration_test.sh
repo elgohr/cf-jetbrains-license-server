ddbd339615d9b7d8e114b69da68f5bac4ceb8d88
Lars Gohr <larsgohr@gmail.com>
2019-03-23 12:36:16 +0100

    Speed up and enhance reliability of integration test

--- integration_test.sh
+++ integration_test.sh
@@ -2,6 +2,17 @@
 set -e
 
 /home/jetbrains/entrypoint.sh &
-sleep 30s
-wget http://localhost:8111 2>&1 | grep Location | grep -q /login
-echo "Server is registered. Everything ok!"
+
+tries=0
+while [ $tries -lt 60 ]
+do
+    if wget http://localhost:8111 2>&1 | grep Location | grep -q /login ; then
+        echo "Server is registered. Everything ok!"
+        exit 0
+    fi
+    tries=`expr $tries + 1`
+    sleep 1s
+done
+
+echo "Server is not registered."
+exit 1
