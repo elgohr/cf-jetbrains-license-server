0c4cd16b49c7aaf3d74c4b1e6cb1b0d451e9b887
Lars Gohr <elgohr@oss.volkswagen.com>
2021-10-01 10:24:06 +0200

    :recycle: Use the health endpoint for making sure that the server is registered

--- integration_test.sh
+++ integration_test.sh
@@ -6,13 +6,20 @@ set -e
 tries=0
 while [ $tries -lt 60 ]
 do
-    if wget http://localhost:8111 2>&1 | grep Location | grep -q /login ; then
+    status_code=$(wget --spider -S "http://localhost:8111/health" 2>&1 | grep "HTTP/" | awk '{print $2}')
+    if [ "${status_code}" == "200" ] ; then
         echo "Server is registered. Everything ok!"
         exit 0
     fi
+
+    echo "Health endpoint is returning error"
+    wget -qO- --content-on-error http://localhost:8111/health || true
+    echo " " # for new line
+
     tries=`expr $tries + 1`
     sleep 1s
 done
 
 echo "Server is not registered."
+wget -qO- --content-on-error http://localhost:8111/health
 exit 1
